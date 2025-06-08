7b3164193ab5f9047dfede8d10a4719f889163f6
yurihp01 <yurihp01@hotmail.com>
2025-06-08 17:21:54 +0100

    Initial Commit

---
+++ economic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  economic
+//
+//  Created by Yuri on 08/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ economic/economicApp.swift
@@ -0,0 +1,17 @@
+//
+//  economicApp.swift
+//  economic
+//
+//  Created by Yuri on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct economicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
