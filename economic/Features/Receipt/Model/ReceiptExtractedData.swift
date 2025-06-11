3c60fba4304ee6a9673bf6a244e8a92891a6b8e8
yurihp01 <yurihp01@hotmail.com>
2025-06-11 18:55:16 +0100

    set to model

---
+++ economic/Features/Receipt/Model/ReceiptExtractedData.swift
@@ -0,0 +1,14 @@
+//
+//  ReceiptExtractedData.swift
+//  economic
+//
+//  Created by Yuri on 11/06/25.
+//
+
+import Foundation
+
+struct ReceiptExtractedData {
+    let date: Date?
+    let amount: Double
+    let currency: String
+}
